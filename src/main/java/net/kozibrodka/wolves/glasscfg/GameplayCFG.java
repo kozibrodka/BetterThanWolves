e75f3beac4a1435539cd9ef4af78dbb1a0b62b95
Atilist <71230342+Atilist@users.noreply.github.com>
2024-12-23 19:23:20 +0100

    Improved Config Descriptions

--- src/main/java/net/kozibrodka/wolves/glasscfg/GameplayCFG.java
+++ src/main/java/net/kozibrodka/wolves/glasscfg/GameplayCFG.java
@@ -1,16 +1,13 @@
 package net.kozibrodka.wolves.glasscfg;
 
 import net.glasslauncher.mods.gcapi3.api.ConfigEntry;
-import org.checkerframework.framework.qual.CFComment;
 
 public class GameplayCFG {
 
-    @ConfigEntry(name = "Gear Box Placement Face")
-    @CFComment("FaceGearBoxAwayFromPlayer")
+    @ConfigEntry(name = "Face Gear Box Away From Player", description = "Face gear boxes away from the player on placement")
     public Boolean faceGearBoxAwayFromPlayer = false;
 
-    @ConfigEntry(name = "Anvil Vanilla Recipes")
-    @CFComment("template_comment")
+    @ConfigEntry(name = "Anvil Vanilla Recipes (game restart required!)", description = "Anvil can be used like a crafting table. Only works in top left corner of the grid")
     public Boolean anvilVanillaRecipes = true;
 
     @ConfigEntry(name = "Deactivate Dung (game restart required!)", description = "Deactivates dung production from wolves and replace it with substitutes in recipes")
