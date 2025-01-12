215f05b0a6f6fcc4c81b7d980d78ea746a23aa02
Atilist <71230342+Atilist@users.noreply.github.com>
2025-01-13 00:32:08 +0100

    Tree Stump Mixins
    Added Mixins to inject Tree Stump Placement into all Tree Types.
    Wool is used as a Placeholder for now.

---
+++ src/main/java/net/kozibrodka/wolves/mixin/OakTreeStumpMixin.java
@@ -0,0 +1,28 @@
+package net.kozibrodka.wolves.mixin;
+
+import net.kozibrodka.wolves.events.ConfigListener;
+import net.minecraft.block.Block;
+import net.minecraft.world.World;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.OakTreeFeature;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Random;
+
+@Mixin(OakTreeFeature.class)
+public abstract class OakTreeStumpMixin extends Feature {
+
+    @Inject(at = @At("TAIL"), method = "generate")
+    private void generateStump(World world, Random random, int x, int y, int z, CallbackInfoReturnable<Boolean> cir) {
+        if (!ConfigListener.wolvesGlass.difficulty.treeStumps) {
+            return;
+        }
+        if (y < 1 || y + 7 > 128) {
+            return;
+        }
+        world.setBlock(x, y, z, Block.WOOL.id);
+    }
+}
