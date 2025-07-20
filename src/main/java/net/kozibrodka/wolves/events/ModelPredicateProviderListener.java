8bd1b12e896322f334ce0f3c3530797d426eaf4c
Ralf2oo2 <ralf2oo2@hotmail.com>
2025-07-20 21:27:05 +0200

    Remove unused import

--- src/main/java/net/kozibrodka/wolves/events/ModelPredicateProviderListener.java
+++ src/main/java/net/kozibrodka/wolves/events/ModelPredicateProviderListener.java
@@ -3,7 +3,6 @@ package net.kozibrodka.wolves.events;
 import net.kozibrodka.wolves.client.model.block.AxleBlockModelPredicateProvider;
 import net.mine_diver.unsafeevents.listener.EventListener;
 import net.modificationstation.stationapi.api.client.event.render.model.BlockModelPredicateProviderRegistryEvent;
-import net.modificationstation.stationapi.api.client.registry.BlockModelPredicateProviderRegistry;
 import net.modificationstation.stationapi.api.mod.entrypoint.Entrypoint;
 import net.modificationstation.stationapi.api.util.Identifier;
 import net.modificationstation.stationapi.api.util.Namespace;
