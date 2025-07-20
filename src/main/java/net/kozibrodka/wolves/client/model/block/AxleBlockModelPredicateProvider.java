34f13d1c7687f6da7e10c9568554d5f8827bdf9a
Ralf2oo2 <ralf2oo2@hotmail.com>
2025-07-20 19:53:51 +0200

    use json model for axle

---
+++ src/main/java/net/kozibrodka/wolves/client/model/block/AxleBlockModelPredicateProvider.java
@@ -0,0 +1,25 @@
+package net.kozibrodka.wolves.client.model.block;
+
+import net.kozibrodka.wolves.block.AxleBlock;
+import net.minecraft.block.Block;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
+import net.modificationstation.stationapi.api.block.BlockState;
+import net.modificationstation.stationapi.api.client.model.block.BlockModelPredicateProvider;
+import org.jetbrains.annotations.Nullable;
+
+public class AxleBlockModelPredicateProvider implements BlockModelPredicateProvider {
+    @Override
+    public float call(BlockState blockState, @Nullable BlockView blockView, @Nullable BlockPos blockPos, int i) {
+        Block block = Block.BLOCKS[blockView.getBlockId(blockPos.x, blockPos.y, blockPos.z)];
+        if(block instanceof AxleBlock axleBlock)
+        {
+            int state = axleBlock.getAxisAlignment(blockView, blockPos.x, blockPos.y, blockPos.z);
+            if(axleBlock.GetPowerLevel(blockView, blockPos.x, blockPos.y, blockPos.z) > 0){
+                state += 3;
+            }
+            return state;
+        }
+        return 0;
+    }
+}
