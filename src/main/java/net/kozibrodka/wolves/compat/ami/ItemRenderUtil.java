f45978d913dc159a438bbc66d3e606dac2439871
Atilist <71230342+Atilist@users.noreply.github.com>
2026-01-17 16:50:33 +0100

    Fix AMI Recipe Block Rendering

--- src/main/java/net/kozibrodka/wolves/compat/ami/ItemRenderUtil.java
+++ src/main/java/net/kozibrodka/wolves/compat/ami/ItemRenderUtil.java
@@ -15,12 +15,12 @@ public class ItemRenderUtil {
         GL11.glEnable(32826);
         GL11.glPushMatrix();
         GL11.glRotatef(120.0F, 1.0F, 0.0F, 0.0F);
-        Lighting.turnOff();
+        Lighting.turnOn();
         GL11.glPopMatrix();
         // Item rendering with scale
         GL11.glPushMatrix();
         GL11.glScalef(scale, scale, scale);
-        ITEM_RENDERER.renderGuiItemDecoration(minecraft.textRenderer, minecraft.textureManager, itemStack, (int) (x / scale), (int) (y / scale));
+        ITEM_RENDERER.renderGuiItem(minecraft.textRenderer, minecraft.textureManager, itemStack, (int) (x / scale), (int) (y / scale));
         GL11.glPopMatrix();
     }
 }
