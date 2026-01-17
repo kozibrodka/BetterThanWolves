0fb3cb451d4ebb017b3ec3e092f9c27506f391ff
Atilist <71230342+Atilist@users.noreply.github.com>
2026-01-17 23:21:01 +0100

    Conveyor Animation and Powering

--- src/main/java/net/kozibrodka/wolves/block/ConveyorBlock.java
+++ src/main/java/net/kozibrodka/wolves/block/ConveyorBlock.java
@@ -1,6 +1,7 @@
 package net.kozibrodka.wolves.block;
 
 import net.kozibrodka.wolves.events.TextureListener;
+import net.kozibrodka.wolves.utils.MechanicalDevice;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.sound.BlockSoundGroup;
@@ -8,7 +9,7 @@ import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.modificationstation.stationapi.api.util.Identifier;
 
-public class ConveyorBlock extends LazyBlockTemplate {
+public class ConveyorBlock extends LazyBlockTemplate implements MechanicalDevice {
     public ConveyorBlock(Identifier identifier, Material material, float hardness, BlockSoundGroup blockSounds) {
         super(identifier, material, hardness, blockSounds);
     }
@@ -47,29 +48,29 @@ public class ConveyorBlock extends LazyBlockTemplate {
             case 8 -> switch (side) {
                 case 0 -> TextureListener.conveyor_bottom;
                 case 1 -> TextureListener.conveyor_up_on;
-                case 2 -> TextureListener.conveyor_front;
-                case 3 -> TextureListener.conveyor_back;
+                case 2 -> TextureListener.conveyor_front_on;
+                case 3 -> TextureListener.conveyor_back_on;
                 default -> TextureListener.conveyor_side_inlet;
             };
             case 9 -> switch (side) {
                 case 0 -> TextureListener.conveyor_bottom;
                 case 1 -> TextureListener.conveyor_down_on;
-                case 2 -> TextureListener.conveyor_back;
-                case 3 -> TextureListener.conveyor_front;
+                case 2 -> TextureListener.conveyor_back_on;
+                case 3 -> TextureListener.conveyor_front_on;
                 default -> TextureListener.conveyor_side_inlet;
             };
             case 10 -> switch (side) {
                 case 0 -> TextureListener.conveyor_bottom;
                 case 1 -> TextureListener.conveyor_left_on;
-                case 4 -> TextureListener.conveyor_front;
-                case 5 -> TextureListener.conveyor_back;
+                case 4 -> TextureListener.conveyor_front_on;
+                case 5 -> TextureListener.conveyor_back_on;
                 default -> TextureListener.conveyor_side_inlet;
             };
             case 11 -> switch (side) {
                 case 0 -> TextureListener.conveyor_bottom;
                 case 1 -> TextureListener.conveyor_right_on;
-                case 4 -> TextureListener.conveyor_back;
-                case 5 -> TextureListener.conveyor_front;
+                case 4 -> TextureListener.conveyor_back_on;
+                case 5 -> TextureListener.conveyor_front_on;
                 default -> TextureListener.conveyor_side_inlet;
             };
             default -> 0;
@@ -93,5 +94,36 @@ public class ConveyorBlock extends LazyBlockTemplate {
         if (facing == 3) {
             world.setBlockMeta(x, y, z, 4);
         }
+    }
+
+    @Override
+    public boolean canOutputMechanicalPower() {
+        return false;
+    }
+
+    @Override
+    public boolean canInputMechanicalPower() {
+        return true;
+    }
+
+    @Override
+    public void powerMachine(World world, int x, int y, int z, int side) {
+        int blockMeta = world.getBlockMeta(x, y, z);
+        if (blockMeta < 6) {
+            world.setBlockMeta(x, y, z, blockMeta + 6);
+        }
+    }
+
+    @Override
+    public void unpowerMachine(World world, int x, int y, int z, int side) {
+        int blockMeta = world.getBlockMeta(x, y, z);
+        if (blockMeta >= 6) {
+            world.setBlockMeta(x, y, z, blockMeta - 6);
+        }
+    }
+
+    @Override
+    public boolean isMachinePowered(World world, int x, int y, int z) {
+        return world.getBlockMeta(x, y, z) >= 6;
     }
 }
