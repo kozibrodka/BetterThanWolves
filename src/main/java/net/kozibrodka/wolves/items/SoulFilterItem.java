60799c62a9f51d97542cb20aec7e342c44807877
Atilist <71230342+Atilist@users.noreply.github.com>
2026-03-03 18:38:20 +0100

    Rework Soul Filtering to eject at the Top

--- src/main/java/net/kozibrodka/wolves/items/SoulFilterItem.java
+++ src/main/java/net/kozibrodka/wolves/items/SoulFilterItem.java
@@ -1,12 +1,23 @@
 package net.kozibrodka.wolves.items;
 
+import net.minecraft.item.ItemStack;
+import net.modificationstation.stationapi.api.client.item.CustomTooltipProvider;
 import net.modificationstation.stationapi.api.template.item.TemplateItem;
 import net.modificationstation.stationapi.api.util.Identifier;
+import org.jetbrains.annotations.NotNull;
 
-public class SoulFilterItem extends TemplateItem {
+public class SoulFilterItem extends TemplateItem implements CustomTooltipProvider {
     public SoulFilterItem(Identifier identifier) {
         super(identifier);
         setMaxCount(1);
         setMaxDamage(128);
+    }
+
+    @Override
+    public @NotNull String[] getTooltip(ItemStack itemStack, String originalTooltip) {
+        return new String[] {
+                originalTooltip,
+                "§7Souls fly away in strong wind"
+        };
     }
 }
