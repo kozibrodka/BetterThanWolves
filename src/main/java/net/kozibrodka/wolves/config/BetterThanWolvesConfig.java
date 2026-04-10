9642b368f86c3dec4507629d8e84f61d0defce2e
kozibrodka <wypas17@gmail.com>
2026-04-10 16:35:37 +0200

    fix v3 april
    add multiplayerSynced = true for Glass CFG to avoid de-sync

--- src/main/java/net/kozibrodka/wolves/config/BetterThanWolvesConfig.java
+++ src/main/java/net/kozibrodka/wolves/config/BetterThanWolvesConfig.java
@@ -3,9 +3,9 @@ package net.kozibrodka.wolves.config;
 import net.glasslauncher.mods.gcapi3.api.ConfigCategory;
 
 public class BetterThanWolvesConfig {
-    @ConfigCategory(name = "§4Difficulty", description = "Gameplay changes to make some mechanics more difficult")
+    @ConfigCategory(name = "§4Difficulty", description = "Gameplay changes to make some mechanics more difficult", multiplayerSynced = true)
     public DifficultyConfig difficulty = new DifficultyConfig();
 
-    @ConfigCategory(name = "§aSmall Tweaks", description = "Gameplay changes that do not affect the difficulty")
+    @ConfigCategory(name = "§aSmall Tweaks", description = "Gameplay changes that do not affect the difficulty", multiplayerSynced = true)
     public SmallTweaksConfig small_tweaks = new SmallTweaksConfig();
 }
